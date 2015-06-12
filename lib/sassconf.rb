3f361cefb43c2a461d5e2c4a36b8a28acf5ba535
Marcel Schlegel <marcel.schlegel@posteo.de>
2015-06-12 11:45:16 +0200

    Improved "live reloading". Improved code.

--- lib/sassconf.rb
+++ lib/sassconf.rb
@@ -15,8 +15,8 @@ module Sassconf
       DESCRIPTION = 'Description:'
                         .newline.blank(3) { 'Adds configuration file to Sass preprocessor.' }
                         .newline.blank(3) { "Version #{Sassconf::VERSION}" }
-                        .newline.blank(3) {'Homepage: http://sassconf.schlegel11.de'}
-                        .newline.blank(3) {'Email: develop@schlegel11.de'}
+                        .newline.blank(3) { 'Homepage: http://sassconf.schlegel11.de' }
+                        .newline.blank(3) { 'Email: develop@schlegel11.de' }
                         .paragraph
       REQUIRED = 'Required:'
       OPTIONAL = 'Optional:'.newline(1, :left)
@@ -70,16 +70,23 @@ module Sassconf
 
   def self.start
     begin
+      cmd_check
       option_args = Parser.parse(ARGV)
       config_manager = ConfigManager.new
       executor = SassExecutor.new(ARGV[0], ARGV[1])
 
-      Sassconf.eval_and_execute(config_manager, executor, option_args)
+      eval_and_execute(config_manager, executor, option_args)
 
       config_manager.watch_update(option_args.config_path, option_args.reload_active) do |filename|
-        logger.info("Config reload: #{filename}")
-        Sassconf.eval_and_execute(config_manager, executor, option_args)
-        puts "Config reloaded: #{filename}".newline(1, :left).paragraph
+        begin
+          logger.info("Config reload: #{filename}")
+          eval_and_execute(config_manager, executor, option_args)
+          puts "Config reloaded: #{filename}".newline(1, :left).paragraph
+        rescue StandardError, ScriptError => e
+          puts e.message
+          logger.error(e)
+          executor.detach_and_kill
+        end
       end
 
       executor.wait
@@ -91,11 +98,22 @@ module Sassconf
     end
   end
 
-  def self.eval_and_execute(config_manager, sass_executor, option_args)
+  def eval_and_execute(config_manager, sass_executor, option_args)
     sass_executor.detach_and_kill
     config_manager.eval_rb_file(option_args.config_path, option_args.extern_args)
     argument_string = sass_executor.create_all_argument_strings(config_manager.variable_with_value_hash, config_manager.variable_hash)
     sass_executor.execute(argument_string)
   end
 
+  def cmd_check
+    message = if Util.windows?
+                '"WMIC" command not found!' unless Util.commands_exist?('WMIC')
+              else
+                '"ps" and/or "pgrep" command not found!' unless Util.commands_exist?('ps', 'pgrep')
+              end
+
+    raise(StandardError, message.newline { 'Please make sure that your system supports the command.' }) if message.string?
+  end
+
+  module_function :eval_and_execute, :cmd_check
 end

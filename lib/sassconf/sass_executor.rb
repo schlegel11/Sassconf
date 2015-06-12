06796fd403a223a446dbfea83b22a19a501ec7b5
Marcel Schlegel <marcel.schlegel@posteo.de>
2015-06-12 11:46:54 +0200

    Add process exist check. Fixed OSX "ps" command.

--- lib/sassconf/sass_executor.rb
+++ lib/sassconf/sass_executor.rb
@@ -29,44 +29,36 @@ module Sassconf
     end
 
     def execute(argument_string)
-      Util.pre_check((argument_string.is_string? and argument_string.is_not_nil_or_empty?), 'Argument string is no string, nil or empty.')
+      Util.pre_check((argument_string.string? and argument_string.not_nil_or_empty?), 'Argument string is no string, nil or empty.')
 
       @pid = spawn(SASS_PROCESS % [argument_string, @sass_input, @sass_output])
       logger.info("Spawn Sass process: #{@pid}")
     end
 
     def detach_and_kill
-      unless @pid.nil?
+      if @pid.integer? && Util.process_exists?(@pid)
         logger.info("Detach Sass process: #{@pid}")
         Process.detach(@pid)
-        out, status = if Util.windows? then
-                        logger.info("Find child processes on MS-DOS")
-                        Open3.capture2("wmic process where (ParentProcessId=#{@pid.to_s}) get ProcessId")
-                      else
-                        logger.info("Find child processes on UNIX")
-                        Open3.capture2('ps', 'h', '--ppid', @pid.to_s, '-o', 'pid')
-                      end
+
         logger.info("Kill process: #{@pid}")
         Process.kill('KILL', @pid)
-        out.each_line do |elem|
-          pid = elem.to_i;
-          unless pid == 0
-            Process.kill('KILL', pid)
-            logger.info("Killed child process: #{pid}")
-          end
+
+        Util.process_childs(@pid) do |pid|
+          Process.kill('KILL', pid)
+          logger.info("Killed child process: #{pid}")
         end
       end
     end
 
     def wait
       logger.info("Wait for Sass process: #{@pid}")
-      Process.wait(@pid) unless @pid.nil?
+      Process.wait(@pid) if @pid.integer? && Util.process_exists?(@pid)
     end
 
     private
     def create_string(argument_type, argument_hash)
-      Util.pre_check(argument_type.is_string?, 'Argument type is no string.')
-      Util.pre_check((argument_hash.is_hash? and !argument_hash.nil?), 'Argument hash is no hash or nil.')
+      Util.pre_check(argument_type.string?, 'Argument type is no string.')
+      Util.pre_check(argument_hash.hash?, 'Argument hash is no hash.')
 
       logger.info("Create argument string from hash: #{argument_hash}")
       argument_hash.each { |key, value| argument_hash[key] = String.empty if value == :no_value }
@@ -74,3 +66,4 @@ module Sassconf
     end
   end
 end
+
